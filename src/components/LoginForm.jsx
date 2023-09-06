6f7728a043d62e0fe7251104ca7ceef0dfaffb0d
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-09-06 20:26:37 -0300

    LOGIN TERMINADO

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -26,11 +28,13 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="card">
-      <div className="card-body">
+    <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src='/img/logo.png' />
+        <Card.Body className='input-container'>
         {loggedIn ? (
           <div>
             <h2>Bienvenido, {username}!</h2>
+            <Button style={{marginTop:'10px', marginBottom: '10px', marginRight:'10px'}}><Link to={'/seleccion'} className='link'>Continuar</Link></Button>
             <button onClick={() => setLoggedIn(false)}>Cerrar sesión</button>
           </div>
         ) : (
@@ -54,13 +58,20 @@ const LoginForm = () => {
                 className="form-control"
               />
             </div>
+            <br/>
             <button onClick={handleLogin} className="btn btn-primary">
               Iniciar sesión
             </button>
-          </div>
-        )}
-      </div>
-    </div>
+            <br/>
+            <div style={{backgroundColor: '#B2BEB5', borderRadius: '10px', marginTop: '10px'}}>
+            <text style={{marginLeft:'10px'}} className='mute'>¿No Tienes una Cuenta?</text> 
+            <Button style={{marginTop:'10px', marginBottom: '10px', marginLeft: '10px'}}><Link to={'/registro'} className='link'>Registrarse</Link></Button>
+            </div>
+         </div>
+        )} 
+      </Card.Body>
+      
+    </Card>
   );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,89 +1,15 @@
-import { Button, Container } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
 import NavBar from '../components/NavBar';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import * as formik from 'formik';
-import * as yup from 'yup';
 import './Login.css'
 import LoginForm from '../components/LoginForm';
 
 function Login() {
-    const { Formik } = formik;
-
-    const schema = yup.object().shape({
-        username: yup.string().required(),
-        password: yup.string().required(),
-    });
 
     return (
         <>
             <NavBar />
             <Container className='card-container'>
                 <LoginForm/>
-                <Card  style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src='/img/logo.png' />
-                    <Card.Body className='input-container'>
-                        <Card.Text>
-                            <Formik
-                                validationSchema={schema}
-                                onSubmit={console.log}
-                                initialValues={{
-                                    username: '',
-                                    password: '',
-                                }}
-                            >
-                                {({ handleSubmit, handleChange, values, touched, errors }) => (
-                                    <Form noValidate onSubmit={handleSubmit}>
-
-                                        <Form.Group
-                                            as={Col}
-                                            md="4"
-                                            controlId="validationFormik101"
-                                            className="position-relative"
-                                        >
-                                            <Form.Label>Usuario</Form.Label>
-                                            <Form.Control
-                                                className='imput-login'
-                                                type="text"
-                                                name="username"
-                                                value={values.username}
-                                                onChange={handleChange}
-                                                isValid={touched.username && !errors.username}
-                                            />
-                                            <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-                                        </Form.Group>
-                                        <Form.Group
-                                            as={Col}
-                                            md="4"
-                                            controlId="validationFormik102"
-                                            className="position-relative"
-                                        >
-                                            <Form.Label>Contraseña</Form.Label>
-                                            <Form.Control
-                                                className='imput-login'
-                                                type="text"
-                                                name="password"
-                                                value={values.password}
-                                                onChange={handleChange}
-                                                isValid={touched.password && !errors.password}
-                                            />
-
-                                            <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-                                        </Form.Group>
-                                        <br />
-                                        <Button type="submit">Submit form</Button>
-                                    </Form>
-                                )}
-                            </Formik>
-
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <text className='mute'>¿No Tienes una Cuenta?</text> <Button><Link to={'/registro'} className='link'>Registrarse</Link></Button>
-                    </Card.Footer>
-                </Card>
             </Container>
         </>
     );
