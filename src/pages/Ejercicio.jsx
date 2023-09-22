4fcf9f39eb7be4bf7711a0562b4ced1e718732b5
Jeronimo Daniel Itzcovich <jditzco@hotmail.com>
2023-09-21 21:30:17 -0300

    sdfsdfds

--- src/pages/Ejercicio.jsx
+++ src/pages/Ejercicio.jsx
@@ -1,73 +1,56 @@
-import { Button, Container, Form, Modal } from 'react-bootstrap'
+import { Button, Container, Form, Modal } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
-import './Ejercicio.css'
-import NavBar from '../components/NavBar'
-import { useEffect, useState } from 'react'
-import preguntasData from '../data/ejercicio'
-/**
- * guardar valores en el estado
- * 
- */
+import './Ejercicio.css';
+import NavBar from '../components/NavBar';
+import { useEffect, useState } from 'react';
 
 const Test = () => {
-
-   // const preguntas = preguntasData
-    const [preguntas, setPreguntas] = useState([])
-    const [respuestas, setRespuestas] = useState([])
-    const [porcentaje, setPorcentaje] = useState(0)
+    const [preguntas, setPreguntas] = useState([]);
+    const [respuestas, setRespuestas] = useState([]);
+    const [porcentaje, setPorcentaje] = useState(0);
     const [show, setShow] = useState(false);
-    const {id} = useParams();
-
-    useEffect(() => async() => {
-        const response = await fetch('http://localhost:5000/preguntas/' + id)
-        const data = await response.json()
-        setPreguntas(data)
-    }, [])
-    
+    const { id } = useParams();
 
     useEffect(() => {
-        let respuestas = []
-        preguntas.forEach(pregunta => {
-            respuestas.push({ idPregunta: pregunta.id, respuesta: null, correcta: null })
-        })
-        setRespuestas(respuestas)
-    }, [])
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/preguntas/${id}`);
+                const data = await response.json();
+                setPreguntas(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-    const calcularPorcentaje = (array) => {
-        let cantidadCorrectas = 0
-        array.forEach(element => {
-            if (element.correcta) cantidadCorrectas++
-        })
-        let porcentaje = Math.round((cantidadCorrectas / array.length) * 100)
-        return porcentaje
-    }
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        const idPregunta = Number(name.split('-')[1])
+        fetchData();
+    }, [id]);
 
-        const nuevasRespuestas = [...respuestas]
-        const index = nuevasRespuestas.findIndex((respuesta) => respuesta.idPregunta === idPregunta)
-        nuevasRespuestas[index].respuesta = Number(value)
-        setRespuestas(nuevasRespuestas)
-    }
     const handleSubmit = (e) => {
-        e.preventDefault()
-        let correctas = respuestas.map(respuesta => {
-            let pregunta = preguntas.find(pregunta => pregunta.id === respuesta.idPregunta)
-            let resp = pregunta.respuestas.find(res => res.id === respuesta.respuesta)
-            return resp.correcta
-        })
-        const nuevasRespuestas = [...respuestas].map((respuesta, index) => {
-            return { ...respuesta, correcta: correctas[index] }
-        })
-        console.log(porcentaje)
-        setRespuestas(nuevasRespuestas)
-        setPorcentaje(calcularPorcentaje(nuevasRespuestas))
-        setShow(true)
-    }
+        e.preventDefault();
+
+        let correctas = respuestas.map((respuesta, index) => {
+            let pregunta = preguntas.find((pregunta) => pregunta.id === respuesta.idPregunta);
+            return pregunta.respuestas.find((res) => res.id === respuesta.respuesta)?.correcta || false;
+        });
+
+        const porcentajeCorrectas = (correctas.filter((correcta) => correcta).length / correctas.length) * 100;
+        setPorcentaje(porcentajeCorrectas);
+
+        setShow(true);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        const idPregunta = Number(name.split('-')[1]);
+
+        const nuevasRespuestas = [...respuestas];
+        const index = nuevasRespuestas.findIndex((respuesta) => respuesta.idPregunta === idPregunta);
+        nuevasRespuestas[index].respuesta = Number(value);
+        setRespuestas(nuevasRespuestas);
+    };
+
     return (
         <>
-        
             <NavBar />
             <Container className='ejercicio-container'>
                 <h1>Suerte!</h1>
@@ -75,44 +58,49 @@ const Test = () => {
                     {preguntas?.map((ej, index) => (
                         <div key={index}>
                             <section className='consigna'>
-                                
                                 <div className='pregunta-container'>
                                     <h3>{ej.pregunta}</h3>
                                 </div>
                             </section>
                             <section className='opciones'>
-                                
                                 <div className='respuestas-container'>
-                                    {
-                                    
-                                        ej.respuestas.map((respuesta, index) => (
-                                            <label className='checks' key={index}>
-                                                <Form.Check className='checks prevent-select' label={respuesta.respuesta} value={respuesta.id} name={`pregunta-${ej.id}`} type={ej.tipo}
-                                            onClick={handleChange} /> 
-                                            </label>
-                                              
-                                        ))
-                                    }
+                                    {ej.respuestas.map((respuesta, index) => (
+                                        <label className='checks' key={index}>
+                                            <Form.Check
+                                                className='checks prevent-select'
+                                                label={respuesta.respuesta}
+                                                value={respuesta.id}
+                                                name={`pregunta-${ej.id}`}
+                                                type={ej.tipo}
+                                                onChange={handleChange}
+                                            />
+                                        </label>
+                                    ))}
                                 </div>
                             </section>
                         </div>
                     ))}
-                    <Button className='button-submit' variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                    <Button className='button-submit' variant='primary' type='submit' onClick={handleSubmit}>
+                        Submit
+                    </Button>
                 </Form>
 
                 <Modal show={show}>
                     <Modal.Header>
                         <Modal.Title>Resultado</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body> <h2>Te sacaste un {porcentaje}/100</h2>
-                    <Button variant="primary"><Link to={'/'} className='link'>Finalizar ejercicio</Link></Button>
+                    <Modal.Body>
+                        <h2>Te sacaste un {porcentaje.toFixed(2)}%</h2>
+                        <Button variant='primary'>
+                            <Link to={'/'} className='link'>
+                                Finalizar ejercicio
+                            </Link>
+                        </Button>
                     </Modal.Body>
-                    
                 </Modal>
-
             </Container>
         </>
-    )
-}
+    );
+};
 
-export default Test
+export default Test;
---
+++ src/pages/ejxantiguo.jsx
@@ -0,0 +1,117 @@
+import { Button, Container, Form, Modal } from 'react-bootstrap'
+import { Link, useParams } from 'react-router-dom';
+import './Ejercicio.css'
+import NavBar from '../components/NavBar'
+import { useEffect, useState } from 'react'
+/**
+ * guardar valores en el estado
+ * 
+ */
+
+const Testo = () => {
+
+   // const preguntas = preguntasData
+    const [preguntas, setPreguntas] = useState([])
+    const [respuestas, setRespuestas] = useState([])
+    const [porcentaje, setPorcentaje] = useState(0)
+    const [show, setShow] = useState(false);
+    const {id} = useParams();
+
+    useEffect(() => async() => {
+        const response = await fetch('http://localhost:5000/preguntas/' + id)
+        const data = await response.json()
+        setPreguntas(data)
+    }, [])
+    
+
+    useEffect(() => {
+        let respuestas = []
+        preguntas.forEach(pregunta => {
+            respuestas.push({ idPregunta: pregunta.id, respuesta: null, correcta: null })
+        })
+        setRespuestas(respuestas)
+    }, [])
+
+    const calcularPorcentaje = (array) => {
+        let cantidadCorrectas = 0
+        array.forEach(element => {
+            if (element.correcta) cantidadCorrectas++
+        })
+        let porcentaje = Math.round((cantidadCorrectas / array.length) * 100)
+        return porcentaje
+    }
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        const idPregunta = Number(name.split('-')[1])
+
+        const nuevasRespuestas = [...respuestas]
+        const index = nuevasRespuestas.findIndex((respuesta) => respuesta.idPregunta === idPregunta)
+        nuevasRespuestas[index].respuesta = Number(value)
+        setRespuestas(nuevasRespuestas)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let correctas = respuestas.map(respuesta => {
+            let pregunta = preguntas.find(pregunta => pregunta.id === respuesta.idPregunta)
+            let resp = pregunta.respuestas.find(res => res.id === respuesta.respuesta)
+            return resp.correcta
+        })
+        const nuevasRespuestas = [...respuestas].map((respuesta, index) => {
+            return { ...respuesta, correcta: correctas[index] }
+        })
+        console.log(porcentaje)
+        setRespuestas(nuevasRespuestas)
+        setPorcentaje(calcularPorcentaje(nuevasRespuestas))
+        setShow(true)
+    }
+    return (
+        <>
+        
+            <NavBar />
+            <Container className='ejercicio-container'>
+                <h1>Suerte!</h1>
+                <Form className='form-respuestas-container'>
+                    {preguntas?.map((ej, index) => (
+                        <div key={index}>
+                            <section className='consigna'>
+                                
+                                <div className='pregunta-container'>
+                                    <h3>{ej.pregunta}</h3>
+                                </div>
+                            </section>
+                            <section className='opciones'>
+                                
+                                <div className='respuestas-container'>
+                                    {
+                                    
+                                        ej.respuestas.map((respuesta, index) => (
+                                            <label className='checks' key={index}>
+                                                <Form.Check className='checks prevent-select' label={respuesta.respuesta} value={respuesta.id} name={`pregunta-${ej.id}`} type={ej.tipo}
+                                            onClick={handleChange} /> 
+                                            </label>
+                                              
+                                        ))
+                                    }
+                                </div>
+                            </section>
+                        </div>
+                    ))}
+                    <Button className='button-submit' variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+                </Form>
+
+                <Modal show={show}>
+                    <Modal.Header>
+                        <Modal.Title>Resultado</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body> <h2>Te sacaste un {porcentaje}/100</h2>
+                    <Button variant="primary"><Link to={'/'} className='link'>Finalizar ejercicio</Link></Button>
+                    </Modal.Body>
+                    
+                </Modal>
+
+            </Container>
+        </>
+    )
+}
+
+export default Testo
